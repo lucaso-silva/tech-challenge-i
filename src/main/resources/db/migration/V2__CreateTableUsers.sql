bcc8f44aaf627d3a55676ac45dece44699218002
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2025-11-28 10:29:30 -0300

    fix collumns name

--- src/main/resources/db/migration/V2__CreateTableUsers.sql
+++ src/main/resources/db/migration/V2__CreateTableUsers.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL,
     login VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    user_type VARCHAR(31) NOT NULL,
     last_modified_date DATE NOT NULL,
     address_id BIGINT NOT NULL,
     FOREIGN KEY (address_id) REFERENCES address (id)
