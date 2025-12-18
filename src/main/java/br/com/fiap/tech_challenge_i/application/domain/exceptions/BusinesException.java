ca47762ea1216f3499499a796e41f29381d8993d
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2025-12-18 10:10:47 -0300

    create BusinesException

---
+++ src/main/java/br/com/fiap/tech_challenge_i/application/domain/exceptions/BusinesException.java
@@ -0,0 +1,9 @@
+package br.com.fiap.tech_challenge_i.application.domain.exceptions;
+
+public class BusinesException extends DomainException {
+
+    public BusinesException(String message) {
+        super(message);
+    }
+
+}
