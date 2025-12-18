6782dec9d7991832597657a784c29f185639c62d
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2025-12-18 10:14:19 -0300

    add ResponseEntity in controller and add create user endpoint

--- src/main/java/br/com/fiap/tech_challenge_i/infastruture/inbound/rest/UserController.java
+++ src/main/java/br/com/fiap/tech_challenge_i/infastruture/inbound/rest/UserController.java
@@ -1,9 +1,13 @@
 package br.com.fiap.tech_challenge_i.infastruture.inbound.rest;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.fiap.tech_challenge_i.application.domain.User;
 import br.com.fiap.tech_challenge_i.application.ports.inbound.ForUserService;
 import br.com.fiap.tech_challenge_i.infastruture.inbound.rest.dtos.UserDetailResponseDTO;
+import br.com.fiap.tech_challenge_i.infastruture.inbound.rest.dtos.UserRequestDTO;
 import br.com.fiap.tech_challenge_i.infastruture.inbound.rest.dtos.UserResponseDTO;
 
 @RestController
@@ -24,16 +29,26 @@ public class UserController {
     }
 
     @GetMapping
-    public List<UserResponseDTO> findAll(@RequestParam(name = "name", required = false) String name) {
-        return forUserService.findByNameLike(name)
-                .stream().map(UserResponseDTO::toDTO).toList();
+    public ResponseEntity<List<UserResponseDTO>> findAll(@RequestParam(name = "name", required = false) String name) {
+        return ResponseEntity.ok(
+                forUserService.findByNameLike(name)
+                        .stream().map(UserResponseDTO::toDTO).toList());
     }
 
     @GetMapping("/{login}")
-    public UserDetailResponseDTO fetchUserByLogin(@PathVariable("login") String login) {
+    public ResponseEntity<UserDetailResponseDTO> fetchUserByLogin(@PathVariable("login") String login) {
 
         User userByLogin = forUserService.findByLogin(login);
-        return UserDetailResponseDTO.toDTO(userByLogin);
+        return ResponseEntity.ok(UserDetailResponseDTO.toDTO(userByLogin));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO requestDTO) {
+
+        User user = forUserService.create(requestDTO.toDomain());
+        URI uri = URI.create("/v1/user/" + user.getLogin());
+        return ResponseEntity.created(uri).body(UserResponseDTO.toDTO(user));
 
     }
 
