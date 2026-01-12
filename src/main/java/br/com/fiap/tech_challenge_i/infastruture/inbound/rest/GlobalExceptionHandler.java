63e41c45e8759283561c3abd091b10634d54aaa9
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-01-12 11:11:31 -0300

    implements rfc7807

--- src/main/java/br/com/fiap/tech_challenge_i/infastruture/inbound/rest/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/tech_challenge_i/infastruture/inbound/rest/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package br.com.fiap.tech_challenge_i.infastruture.inbound.rest;
 
-import br.com.fiap.tech_challenge_i.application.domain.exceptions.BusinessException;
-import br.com.fiap.tech_challenge_i.application.domain.exceptions.NotFoundException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,36 +15,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+import br.com.fiap.tech_challenge_i.application.domain.exceptions.BusinessException;
+import br.com.fiap.tech_challenge_i.application.domain.exceptions.NotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = { NotFoundException.class })
-    protected ResponseEntity<Object> handleNotFound(final NotFoundException ex, final WebRequest request) {
-        return handleExceptionInternal(ex,
-                ex.getMessage(),
-                new HttpHeaders(),
-                HttpStatus.NOT_FOUND,
-                request);
+    protected ResponseEntity<ProblemDetail> handleNotFound2(final NotFoundException ex, final WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setType(URI.create("https://example.com/not-found"));
+        problemDetail.setTitle("Not Found");
+        problemDetail.setInstance(URI.create(
+                request.getDescription(false).replace("uri=", "")));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
     }
 
     @ExceptionHandler(value = { BusinessException.class })
     protected ResponseEntity<Object> handleBusinessException(final BusinessException ex, final WebRequest request) {
-        return handleExceptionInternal(ex,
-                ex.getMessage(),
-                new HttpHeaders(),
-                HttpStatus.BAD_REQUEST,
-                request);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+        problemDetail.setType(URI.create("https://example.com/business-error"));
+        problemDetail.setTitle("Business Error");
+        problemDetail.setInstance(URI.create(request.getDescription(false).replace("uri=", "")));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
     }
 
     @ExceptionHandler(value = { Exception.class })
     protected ResponseEntity<Object> handleGeneralException(final RuntimeException ex, final WebRequest request) {
-        return handleExceptionInternal(ex,
-                "An unexpected error occurred",
-                new HttpHeaders(),
-                HttpStatus.INTERNAL_SERVER_ERROR, request);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                "An unexpected error occurred");
+        problemDetail.setType(URI.create("https://example.com/internal-server-error"));
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setInstance(URI.create(request.getDescription(false).replace("uri=", "")));
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetail);
     }
 
     @Override
@@ -49,12 +58,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             MethodArgumentNotValidException ex,
             HttpHeaders headers,
             HttpStatusCode status,
-            WebRequest request){
+            WebRequest request) {
 
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors()
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation failed");
+        problemDetail.setType(URI.create("https://example.com/validation-error"));
+        problemDetail.setTitle("Validation Error");
+        problemDetail.setInstance(URI.create(request.getDescription(false).replace("uri=", "")));
+        problemDetail.setProperty("invalid-params", errors);
+        return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
     }
 }
