f769c8a76d3f87b3f139517276555984c00d48b9
Lucas Oliveira <luc.osilva@gmail.com>
2026-01-19 19:41:47 -0300

    remove unused dtos

--- src/main/java/br/com/fiap/tech_challenge_i/infrastructure/inbound/rest/dtos/ErrorResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.fiap.tech_challenge_i.infrastructure.inbound.rest.dtos;
-
-public record ErrorResponseDTO(String message) {
-}
--- src/main/java/br/com/fiap/tech_challenge_i/infrastructure/inbound/rest/dtos/ValidateLoginRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.tech_challenge_i.infrastructure.inbound.rest.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ValidateLoginRequestDTO(@NotBlank(message = "The user's login can't be empty") String login,
-                                      @NotBlank(message = "Password can't be empty") String password) {
-}
